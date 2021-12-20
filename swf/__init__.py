7278a21bd7dd42f680b22d29c75e5d47c7c46afe
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-20 01:21:20 +0000

    add swf file

---
+++ swf/__init__.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from typing import List
+
+
+from swf.stream import Stream
+from swf.header import Header
+
+
+class UnmatchedFileLength(Exception):
+    pass
+
+
+@dataclass
+class File:
+    header: Header
+    tags: List[object]
+
+    @classmethod
+    def unpack(cls, stream):
+        header = Header.unpack(stream)
+
+        position = stream.byte_position
+        data = stream.bytes_buffers
+
+        if header.is_zlib_compressed:
+            import zlib
+            data = zlib.decompress(data)
+        elif header.is_lzma_compressed:
+            import pylzma
+            data = pylzma.decompress(data)
+
+        if position + len(data) != header.file_length:
+            raise UnmatchedFileLength()
+
+        stream = Stream(data)
+        # we don't read all the header struct data in the first
+        # unpack since the data might be compressed
+        header.unpack_rest(reader)
+
+        return cls(
+            header=header,
+            tags=[],
+        )
+
+
+def parse(path):
+    with open(path, 'rb') as file:
+        data = file.read()
+        stream = Stream(data)
+
+    swf = File.unpack(stream)
+    return swf
