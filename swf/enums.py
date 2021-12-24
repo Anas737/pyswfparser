f091d980178550d2e771ee7a4f99cfdd0c8e4a9f
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-24 20:51:16 +0000

    add more enums

--- swf/enums.py
+++ swf/enums.py
@@ -7,3 +7,22 @@ class LangagueCode(enum):
     KOREAN = 3
     SIMPLIFIED_CHINESE = 4
     TRADITIONAL_CHINESE = 5
+
+
+class ValueType(enum):
+    STRING = 0
+    FLOAT = 1
+    NULL = 2
+    UNDEFINED = 3
+    REGISTER = 4
+    BOOL = 5
+    DOUBLE = 6
+    INTEGER = 7
+    CONSTANT_8 = 8
+    CONSTANT_16 = 9
+
+
+class VarsMethod(enum):
+    NONE: 0
+    GET: 1
+    POST: 2
