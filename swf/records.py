eb9d42bc901eced07b4570470310fa93f24d2e0d
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-26 11:51:54 +0000

    update records

--- swf/records.py
+++ swf/records.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
+from typing import Union
 
-from swf.actions import Action
-from swf.actions import unpack as unpack_action
-from swf.filters import Filter, unpack as unpack_filter
+from swf.enums import CapStyleType, FillStyleType, JoinStyleType
 
 
 @dataclass
@@ -334,70 +333,581 @@ class Events:
 
 
 @dataclass
-class ClipAction:
-    events: Events
-    key_code: int
-    actions: list[Action]
+class Grad:
+    ratio: int
+    color: RGB
 
     @classmethod
-    def unpack(cls, version, stream):
-        events = Events.unpack(version, stream)
-        size = stream.read_uint32()
-        key_code = None
-        if events.is_key_press:
-            key_code = stream.read_uint8()
-            size = size - 1
-
-        actions = []
-        position = stream.byte_position
-        while stream.byte_position < position + size:
-            actions.append(unpack_action(stream))
+    def unpack(cls, shape_version, stream):
+        ratio = stream.read_uint8()
+        
+        if shape_version <= 2:
+            color = RGB.unpack(stream)
+        else:
+            color = RGBA.unpack(stream)
 
         return cls(
-            events=events,
-            key_code=key_code,
-            actions=actions,
+            ratio=ratio,
+            color=color,
+        )
+
+@dataclass
+class Gradient:
+    spread_mode: int
+    imterpolation_mode: RGB
+    grads: list[Grad]
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        spread_mode = stream.read_ubits(2)
+        imterpolation_mode = stream.read_ubits(2)
+        count = stream.read_ubits(4)
+        grads = [Grad.unpack(shape_version, stream)
+                for _ in range(count)]
+
+        return cls(
+            spread_mode=spread_mode,
+            imterpolation_mode=imterpolation_mode,
+            grads=grads,
         )
 
 
 @dataclass
-class ClipActions:
-    # reserved: int
-    events: Events
-    clip_actions: list[ClipAction]
-    clip_action_end: int
+class FocalGradient:
+    spread_mode: int
+    imterpolation_mode: RGB
+    grads: list[Grad]
+    focal_point: float
 
     @classmethod
-    def unpack(cls, version, stream):
-        stream.read_uint16()  # reserved always 0
-        events = Events.unpack(version, stream)
-        clip_actions = [ClipAction.unpack(version, stream)
-                        for _ in range(len(events))]
-
-        clip_action_end = 0
-        if version <= 5:
-            clip_action_end = stream.read_uint16()
-        elif version >= 6:
-            clip_action_end = stream.read_uint32()
+    def unpack(cls, shape_version, stream):
+        spread_mode = stream.read_ubits(2)
+        imterpolation_mode = stream.read_ubits(2)
+        count = stream.read_ubits(4)
+        grads = [Grad.unpack(shape_version, stream)
+                for _ in range(count)]
+        focal_point = stream.read_fixed8()
 
         return cls(
-            events=events,
-            clip_actions=clip_actions,
-            clip_action_end=clip_action_end,
+            spread_mode=spread_mode,
+            imterpolation_mode=imterpolation_mode,
+            grads=grads,
+            focal_point=focal_point,
         )
 
 
 @dataclass
-class FilterList:
-    filters: list[Filter]
+class FillStyle:
+    type: int
+    color: RGB
+    gradient_matrix: Matrix
+    gradient: Gradient
+    bitmap_id: int
+    bitmap_matrix: Matrix
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        type = FillStyleType(stream.read_uint8())
+        color = None
+        gradient_matrix = None
+        gradient = None
+        bitmap_id = None
+        bitmap_matrix= None
+        if type == FillStyleType.SOLID:
+            if shape_version <= 2:
+                color = RGB.unpack(stream)
+            else:
+                color = RGBA.unpack(stream)
+        elif type in (
+                        FillStyleType.LINEAR_GRADIENT,
+                        FillStyleType.RADIAL_GRADIENT,
+                        FillStyleType.FOCAL_RADIAL_GRADIENT,
+                    ):
+            gradient_matrix = Matrix.unpack(stream)
+            if type == FillStyleType.FOCAL_RADIAL_GRADIENT:
+                gradient =  FocalGradient.unpack(stream)
+            else:
+                gradient =  Gradient.unpack(stream)
+        elif type in (
+                        FillStyleType.REPEATING_BITMAP,
+                        FillStyleType.CLIPPED_BITMAP,
+                        FillStyleType.NON_SMOOTHED_REPEATING_BITMAP,
+                        FillStyleType.NON_SMOOTHED_CLIPPED_BITMAP,
+                    ):
+            bitmap_id = stream.read_uint16()
+            bitmap_matrix = Matrix.unpack(stream)
+
+        return cls(
+            type=type,
+            color=color,
+            gradient_matrix=gradient_matrix,
+            gradient=gradient,
+            bitmap_id=bitmap_id,
+            bitmap_matrix=bitmap_matrix,
+        )
+
+
+@dataclass
+class FillStyleArray:
+    fill_styles: list[FillStyle]
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        count = stream.read_uint8()
+        if count == 0xff:
+            count = stream.read_uint16()
+        fill_styles = [FillStyle.unpack(shape_version, stream)
+                        for _ in range(count)]
+        return cls(
+            fill_styles=fill_styles,
+        )
+
+@dataclass
+class LineStyle:
+    width: int
+    color: RGB
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        width = stream.read_uint16()
+
+        if shape_version <= 2:
+            color = RGB.unpack(stream)
+        else:
+            color = RGBA.unpack(stream)
+
+        return cls(
+            width=width,
+            color=color,
+        )
+
+
+class ShapeRecord:
+    pass
+
+
+def unpack_shape(stream):
+    is_edge_record = stream.read_bit_bool()
+    if is_edge_record:
+        is_straight = stream.read_bit_bool()
+        if is_straight:
+            return StraightEdge.unpack(stream)
+        else:
+            return CurvedEdge.unpack(stream)
+
+    flags = stream.tell_bits(5)
+    if flags == 0:
+        return EndShape.unpack(stream)
+
+    return StyleChange.unpack(stream)
+
+@dataclass
+class StyleChange(ShapeRecord):
+    is_edge_record: bool
+    move_delta_x: int
+    move_delta_y: int
+    fill_style_0: int
+    fill_style_1: int
+    line_style: int
+    fill_styles: int
+    line_styles: int
+
+    @classmethod
+    def unpack(cls, stream):
+        state_new_styles = stream.read_bit_bool()
+        state_line_style = stream.read_bit_bool()
+        state_fill_style_1 = stream.read_bit_bool()
+        state_fill_style_0 = stream.read_bit_bool()
+        state_move_to = stream.read_bit_bool()
+
+        move_delta_x = None
+        move_delta_y = None
+        if state_move_to:
+            move_bits = stream.read_ubits(5)
+            move_delta_x = stream.read_sbits(move_bits)
+            move_delta_y = stream.read_sbits(move_bits)
+
+        fill_styles = None
+        line_styles = None
+        fill_bits = 0
+        line_bits = 0
+        if state_new_styles:
+            fill_styles = LineStyleArray.unpack(stream)
+            line_styles = LineStyleArray.unpack(stream)
+            fill_bits = stream.read_ubits(4)
+            line_bits = stream.read_ubits(4)
+
+        fill_style_0 = None
+        if state_fill_style_0 and fill_bits:
+            fill_style_0 = stream.read_ubits(fill_bits)
+
+        fill_style_1 = None
+        if state_fill_style_1 and fill_bits:
+            fill_style_1 = stream.read_ubits(fill_bits)
+
+        line_style = None
+        if state_line_style and line_bits:
+            line_style = stream.read_ubits(line_bits)
+
+        return cls(
+            is_edge_record=False,
+            move_delta_x=move_delta_x,
+            move_delta_y=move_delta_y,
+            fill_style_0=fill_style_0,
+            fill_style_1=fill_style_1,
+            line_style=line_style,
+            fill_styles=fill_styles,
+            line_styles=line_styles,
+        )
+
+
+@dataclass
+class StraightEdge(ShapeRecord):
+    is_edge_record: bool
+    is_straight: bool
+    general_line_flag: bool
+    delta_x: int
+    delta_y: int
+    vert_line_flag: bool
+
+    @classmethod
+    def unpack(cls, stream):
+        bits = stream.read_ubits(4)
+        general_line_flag = stream.read_bit_bool()
+        delta_x = 0
+        delta_y = 0
+        vert_line_flag = False
+        if general_line_flag:
+            delta_x = stream.read_sbits(bits + 2)
+            delta_y = stream.read_sbits(bits + 2)
+            vert_line_flag = stream.read_bit_bool()
+            if vert_line_flag:
+                delta_x = stream.read_sbits(bits + 2)
+                delta_y = stream.read_sbits(bits + 2)
+
+        return cls(
+            is_edge_record=True,
+            is_straight=True,
+            general_line_flag=general_line_flag,
+            delta_x=delta_x,
+            delta_y=delta_y,
+            vert_line_flag=vert_line_flag,
+        )
+
+
+@dataclass
+class CurvedEdge(ShapeRecord):
+    is_edge_record: bool
+    is_straight: bool
+    control_delta_x: int
+    control_delta_y: int
+    anchor_delta_x: int
+    anchor_delta_y: int
+
+    @classmethod
+    def unpack(cls, stream):
+        bits = stream.read_ubits(4)
+        control_delta_x = stream.read_sbits(bits + 2)
+        control_delta_y = stream.read_sbits(bits + 2)
+        anchor_delta_x = stream.read_sbits(bits + 2)
+        anchor_delta_y = stream.read_sbits(bits + 2)
+
+        return cls(
+            is_edge_record=True,
+            is_straight=False,
+            control_delta_x=control_delta_x,
+            control_delta_y=control_delta_y,
+            anchor_delta_x=anchor_delta_y,
+            anchor_delta_y=anchor_delta_y,
+        )
+
+
+@dataclass
+class EndShape(ShapeRecord):
+    is_edge_record: bool
+    end_of_shape: int
+
+    @classmethod
+    def unpack(cls, stream):
+        end_of_shape = stream.read_ubits(5)
+
+        return cls(
+            is_edge_record=False,
+            end_of_shape=end_of_shape,
+        )
+
+@dataclass
+class Shape:
+    fill_bits: int
+    line_bits: int
+    shape_records: list[ShapeRecord]
+
+    @classmethod
+    def unpack(cls, stream):
+        fill_bits = stream.read_ubits(4)
+        line_bits = stream.read_ubits(4)
+        shape_records = []
+        shape = unpack_shape(stream)
+        while not isinstance(shape, EndShape):
+            shape_records.append(shape)
+            shape = unpack_shape(stream)
+
+        return cls(
+            fill_bits=fill_bits,
+            line_bits=line_bits,
+            shape_records=shape_records,
+        )
+
+
+@dataclass
+class LineStyleArray:
+    line_styles: list[LineStyle]
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        count = stream.read_uint8()
+        if count == 0xff:
+            count = stream.read_uint16()
+        line_styles = [LineStyle.unpack(shape_version, stream)
+                        for _ in range(count)]
+        
+        return cls(
+            line_styles=line_styles,
+        )
+
+
+@dataclass
+class ShapeWithStyle(Shape):
+    fill_styles: FillStyleArray
+    line_styles: LineStyleArray
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        fill_styles = FillStyleArray.unpack(shape_version, stream)
+        line_styles = LineStyleArray.unpack(shape_version, stream)
+        shape = Shape.unpack(stream)
+
+        return cls(
+            fill_bits=shape.fill_bits,
+            line_bits=shape.line_bits,
+            shape_records=shape.shape_records,
+            fill_styles=fill_styles,
+            line_styles=line_styles,
+        )
+
+
+@dataclass
+class MorphGradRecord:
+    start_ratio: int
+    start_color: RGBA
+    end_ratio: int
+    end_color: RGBA
+
+    @classmethod
+    def unpack(cls, stream):
+        start_ratio = stream.read_uint8()
+        start_color = RGBA.unpack(stream)
+        end_ratio = stream.read_uint8()
+        end_color = RGBA.unpack(stream)
+
+        return cls(
+            start_ratio=start_ratio,
+            start_color=start_color,
+            end_ratio=end_ratio,
+            end_color=end_color,
+        )
+
+
+@dataclass
+class MorphGradient:
+    morph_grads: list[MorphGradRecord]
 
     @classmethod
     def unpack(cls, stream):
         count = stream.read_uint8()
-        filters = [unpack_filter(stream) for _ in range(count)]
+        morph_grads = [MorphGradRecord.unpack(stream) 
+                        for _ in range(count)]
 
         return cls(
-            filters=filters,
+            morph_grads=morph_grads,
         )
 
+@dataclass
+class MorphFillStyle:
+    type: int
+    start_color: RGBA
+    end_color: RGBA
+    start_gradient_matrix: Matrix
+    end_gradient_matrix: Matrix
+    gradient: MorphGradient
+    bitmap_id: int
+    start_bitmap_matrix: Matrix
+    end_bitmap_matrix: Matrix
+
+    @classmethod
+    def unpack(cls, stream):
+        type = FillStyleType(stream.read_uint8())
+        start_color = None
+        end_color = None
+        start_gradient_matrix = None
+        end_gradient_matrix = None
+        gradient = None
+        bitmap_id = None
+        start_bitmap_matrix = None
+        end_bitmap_matrix = None
+        if type == FillStyleType.SOLID:
+            start_color = RGBA.unpack(stream)
+            end_color = RGBA.unpack(stream)
+        elif type in (
+                        FillStyleType.LINEAR_GRADIENT,
+                        FillStyleType.RADIAL_GRADIENT,
+                    ):
+            start_gradient_matrix = Matrix.unpack(stream)
+            end_gradient_matrix = Matrix.unpack(stream)
+            gradient =  MorphGradient.unpack(stream)
+        elif type in (
+                        FillStyleType.REPEATING_BITMAP,
+                        FillStyleType.CLIPPED_BITMAP,
+                        FillStyleType.NON_SMOOTHED_REPEATING_BITMAP,
+                        FillStyleType.NON_SMOOTHED_CLIPPED_BITMAP,
+                    ):
+            bitmap_id = stream.read_uint16()
+            start_bitmap_matrix = Matrix.unpack(stream)
+            end_bitmap_matrix = Matrix.unpack(stream)
+
+        return cls(
+            type=type,
+            start_color=start_color,
+            end_color=end_color,
+            start_gradient_matrix=start_gradient_matrix,
+            end_gradient_matrix=end_gradient_matrix,
+            gradient=gradient,
+            bitmap_id=bitmap_id,
+            start_bitmap_matrix=start_bitmap_matrix,
+            end_bitmap_matrix=end_bitmap_matrix,
+        )
+
+
+@dataclass
+class MorphLineStyle:
+    start_width: int
+    end_width: int
+    start_color: RGBA
+    end_color: RGBA
+
+
+    @classmethod
+    def unpack(cls, stream):
+        start_width = stream.read_uint16()
+        end_width = stream.read_uint16()
+        start_color = RGBA.unpack(stream)
+        end_color = RGBA.unpack(stream)
+
+        return cls(
+            start_width=start_width,
+            end_width=end_width,
+            start_color=start_color,
+            end_color=end_color,
+        )
+
+
+@dataclass
+class MorphLineStyle2:
+    start_width: int
+    end_width: int
+    start_cap_style: CapStyleType
+    join_style: JoinStyleType
+    has_fill: bool
+    no_h_scale: bool
+    no_v_scale: bool
+    pixel_hinting: bool
+    #reserved: int
+    no_close: bool
+    end_cap_style: CapStyleType
+    miter_limit_factor: int
+    start_color: RGBA
+    end_color: RGBA
+    fill_type: MorphFillStyle
+
+    @classmethod
+    def unpack(cls, stream):
+        start_width = stream.read_uint16()
+        end_width = stream.read_uint16()
+        start_cap_style = CapStyleType(stream.read_ubit(2))
+        join_style = JoinStyleType(stream.read_ubit(2))
+        has_fill = stream.read_bit_bool()
+        no_h_scale = stream.read_bit_bool()
+        no_v_scale = stream.read_bit_bool()
+        pixel_hinting = stream.read_bit_bool()
+        stream.read_ubits(5)  # reserved
+        no_close = stream.read_bit_bool()
+        end_cap_style = CapStyleType(stream.read_ubit(2))
+
+        miter_limit_factor = None
+        if join_style == JoinStyleType.MITER:
+            miter_limit_factor = stream.read_uint16()
+
+        start_color = None
+        end_color = None
+        fill_stype = None
+        if has_fill:
+            fill_type = MorphFillStyle.unpack(stream)
+        else:
+            start_color = RGBA.unpack(stream)
+            end_color = RGBA.unpack(stream)
+
+        return cls(
+            start_width=start_width,
+            end_width=end_width,
+            start_cap_style=start_cap_style,
+            join_style=join_style,
+            has_fill=has_fill,
+            no_h_scale=no_h_scale,
+            no_v_scale=no_v_scale,
+            pixel_hinting=pixel_hinting,
+            no_close=no_close,
+            end_cap_style=end_cap_style,
+            miter_limit_factor=miter_limit_factor,
+            start_color=start_color,
+            end_color=end_color,
+            fill_type=fill_type,
+        )
+
+
+@dataclass
+class MorphLineStyleArray:
+    line_styles: list[Union[MorphLineStyle, MorphLineStyle2]]
+
+    @classmethod
+    def unpack(cls, shape_version, stream):
+        count = stream.read_uint8()
+        if count == 0xff:
+            count = stream.read_uint16()
+        line_styles = [
+            MorphLineStyle.unpack(stream) if shape_version == 1 else \
+            MorphLineStyle2.unpack(stream)
+            for _ in range(count)
+        ]
+
+        return cls(
+            line_styles=line_styles,
+        )
+
+
+@dataclass
+class MorphFillStyleArray:
+    line_styles: list[MorphFillStyle]
+
+    @classmethod
+    def unpack(cls, stream):
+        count = stream.read_uint8()
+        if count == 0xff:
+            count = stream.read_uint16()
+        line_styles = [
+            MorphFillStyle.unpack(stream) for _ in range(count)
+        ]
+
+        return cls(
+            line_styles=line_styles,
+        )
 
