f6dad711d3b5f570c9242e04319c6df8893250a0
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-27 11:28:26 +0000

    add byte_align_unpack decorator

--- swf/__init__.py
+++ swf/__init__.py
@@ -1,55 +1,11 @@
-from dataclasses import dataclass
+def byte_align_unpack(unpack):
+    def _unpack(*args):
+        if isinstance(args, tuple):
+            *_, stream = args
+        else:
+            stream = args
 
-from swf.stream import Stream
-from swf.exceptions import UnmatchedFileLength
-from swf.header import Header
-from swf.tags import End, Tag, unpack as unpack_tag
+        unpack(*args)
+        stream.byte_align()
 
-
-@dataclass
-class File:
-    header: Header
-    tags: list[Tag]
-
-    @classmethod
-    def unpack(cls, stream):
-        header = Header.unpack(stream)
-
-        position = stream.byte_position
-        data = stream.available_bytes
-
-        if header.is_zlib_compressed:
-            import zlib
-            data = zlib.decompress(data)
-        elif header.is_lzma_compressed:
-            import pylzma
-            data = pylzma.decompress(data)
-
-        if position + len(data) != header.file_length:
-            raise UnmatchedFileLength()
-
-        stream = Stream(data)
-        # we don't read all the header struct data in the first
-        # unpack since the data might be compressed
-        header.unpack_rest(stream)
-
-        tags = []
-        tag = unpack_tag(header.version, stream)
-        while not isinstance(tag, End):
-            # print(tag)
-            tags.append(tag)
-            tag = unpack_tag(header.version, stream)
-
-        return cls(
-            header=header,
-            tags=tags,
-        )
-
-
-def parse(path):
-    with open(path, 'rb') as file:
-        data = file.read()
-        stream = Stream(data)
-
-    swf = File.unpack(stream)
-    return swf
+    return _unpack
