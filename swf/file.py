0d75ee3855c2f399852f41a6baa8ff2cbe22700f
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-27 11:27:56 +0000

    move header and parse to file module

--- swf/file.py
+++ swf/file.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from swf.stream import Stream
+from swf.exceptions import UnmatchedFileLength
+from swf.tags import End, Tag, unpack as unpack_tag
 from swf.records import Rectangle
 
 
@@ -73,3 +76,51 @@ class Header:
         self.frame_size = Rectangle.unpack(stream)
         self.frame_rate = stream.read_uint16()
         self.frame_count = stream.read_uint16()
+
+
+@dataclass
+class File:
+    header: Header
+    tags: list[Tag]
+
+    @classmethod
+    def unpack(cls, stream):
+        header = Header.unpack(stream)
+
+        position = stream.byte_position
+        data = stream.available_bytes
+
+        if header.is_zlib_compressed:
+            import zlib
+            data = zlib.decompress(data)
+        elif header.is_lzma_compressed:
+            import pylzma
+            data = pylzma.decompress(data)
+
+        if position + len(data) != header.file_length:
+            raise UnmatchedFileLength()
+
+        stream = Stream(data)
+        # we don't read all the header struct data in the first
+        # unpack since the data might be compressed
+        header.unpack_rest(stream)
+
+        tags = []
+        tag = unpack_tag(header.version, stream)
+        while not isinstance(tag, End):
+            tags.append(tag)
+            tag = unpack_tag(header.version, stream)
+
+        return cls(
+            header=header,
+            tags=tags,
+        )
+
+
+def parse(path):
+    with open(path, 'rb') as file:
+        data = file.read()
+        stream = Stream(data)
+
+    swf = File.unpack(stream)
+    return swf
