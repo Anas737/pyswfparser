18267578f5c3751d41fcb697c319d149a43eab6b
Anas El Boukhari <elboukhari.anas.95@gmail.com>
2021-12-29 11:14:37 +0000

    add multiname kind enum

--- amv2/enums.py
+++ amv2/enums.py
@@ -9,3 +9,16 @@ class NamespaceKind(Enum):
     explicit_ns = 0x19
     static_protected_ns = 0x1a
     private_ns = 0x05
+
+
+class MultinameKind(Enum):
+    q_name = 0x07
+    q_name_a = 0x0d
+    rt_q_name = 0x0f
+    rt_q_name_a = 0x10
+    rt_q_name_l = 0x11
+    rt_q_name_l_a = 0x11
+    multiname = 0x09
+    multiname_a = 0x0e
+    multiname_l = 0x1b
+    multiname_l_a = 0x1c
